fb1f34ad491588088dc584826a688273c09c9ba6
ariadnana <ariadnaxm@gmail.com>
2017-01-20 20:48:58 +0100

    el mapa mostra d'inici catalunya però de moment no te cap marcador

--- Engrescat/app/src/main/java/com/example/ariadna/engrescat/MapActivity.java
+++ Engrescat/app/src/main/java/com/example/ariadna/engrescat/MapActivity.java
@@ -3,6 +3,7 @@ package com.example.ariadna.engrescat;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -31,9 +32,10 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng centre = new LatLng(41.918629, 2.254944);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(centre));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 7.5f) );
     }
 
     public void obrellista(View view){
